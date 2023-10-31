f9dcea732c3c023644ff53d3d1618afc6e765309
Abir <abirmahmud221@gmail.com>
2023-10-31 16:04:20 +0600

    added the browser cookies

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import img from "../../assets/images/login/login.svg";
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProviders";
+import axios from "axios";
 
 const Login = () => {
 
@@ -18,9 +19,18 @@ const Login = () => {
     console.log( email, password);
     signIn(email, password)
     .then(result => {
-      const user = result.user;
-      console.log(user);
-      navigate(location?.state ? location?.state : '/')
+      const loggedInUser = result.user;
+      console.log(loggedInUser);
+      const user = { email };
+      // get access token 
+      axios.post('http://localhost:5000/jwt', user, {withCredentials: true})
+      .then(res => {
+        console.log(res.data)
+        if(res.data.success){
+          navigate(location?.state ? location?.state : '/')
+        }
+      })
+
     })
     .catch(error => console.log(error))
   };

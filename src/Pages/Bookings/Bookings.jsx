6a40ca212b0eb5c1b0d6d6340ea7a0d4d37e50c2
Abir <abirmahmud221@gmail.com>
2023-11-02 19:54:00 +0600

    61-6

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProviders";
 import BookingRow from "./BookingRow";
-import axios from "axios";
+// import axios from "axios";
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
@@ -9,13 +9,17 @@ const Bookings = () => {
 
   const url = `http://localhost:5000/bookings?email=${user?.email}`;
   useEffect(() => {
-    axios.get(url, {withCredentials: true})
-    .then(res => {
-      setBookings(res.data); 
-    })
+    // axios.get(url, {withCredentials: true})
+    // .then(res => {
+    //   setBookings(res.data); 
+    // })
+
     // fetch(url)
     //   .then((res) => res.json())
     //   .then((data) => setBookings(data));
+    fetch((url), {credentials: 'include'})
+    .then(res => res.json())
+    .then(data => setBookings(data))
   }, [url]); 
 
   const handleDelete = id => {
